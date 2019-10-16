464c1fa2edcf29478a542aacaeaebe29f04ff999
Ban Vuong <vuongdieuban@gmail.com>
2019-10-15 22:22:35 -0600

    google login/logout. Able to acquire access token from google login

--- frontend/src/components/mainpage.js
+++ frontend/src/components/mainpage.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import Input from "./input";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 class MainPage extends Component {
   state = {};
+  responseGoogle = res => {
+    console.log("Google Login Response:", res);
+  };
+
+  logoutGoogle = () => {
+    console.log("Google Logout");
+  };
+
   render() {
     return (
       <React.Fragment>
-        <div className="search_form">
+        <div className="align-items-center">
           <Input />
+          <div>
+            <GoogleLogin
+              clientId={process.env.REACT_APP_CLIENT_ID}
+              buttonText="Login"
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+            <GoogleLogout
+              clientId={process.env.REACT_APP_CLIENT_ID}
+              buttonText="Logout"
+              onLogoutSuccess={this.logoutGoogle}
+            />
+          </div>
         </div>
       </React.Fragment>
     );
