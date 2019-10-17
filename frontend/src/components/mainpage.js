2cf7c56b2dd311374215ca45b0c4ee8d82d536c7
Ban Vuong <vuongdieuban@gmail.com>
2019-10-16 18:14:34 -0600

    display signin/signout based on current status

--- frontend/src/components/mainpage.js
+++ frontend/src/components/mainpage.js
@@ -5,7 +5,9 @@ import auth from "../services/authService";
 import user from "../services/userService";
 
 class MainPage extends Component {
-  state = {};
+  state = {
+    currentUser: null
+  };
   responseGoogle = async res => {
     const { error } = res;
     if (error)
@@ -16,32 +18,43 @@ class MainPage extends Component {
     await auth.signinUser(res.Zi.access_token);
     const userInfo = await user.getUserInfo();
     console.log(userInfo);
+    this.setState({ currentUser: userInfo });
     // window.location = "/me";
   };
 
   signoutGoogle = () => {
     console.log("Google Logout");
     auth.signoutUser();
+    this.setState({ currentUser: null });
   };
 
+  componentDidMount() {
+    const currentUser = auth.getCurrentUser();
+    this.setState({ currentUser });
+  }
+
   render() {
+    const { currentUser } = this.state;
     return (
       <React.Fragment>
         <div className="align-items-center">
           <Input />
           <div>
-            <GoogleLogin
-              clientId={process.env.REACT_APP_CLIENT_ID}
-              buttonText="Sign In"
-              onSuccess={this.responseGoogle}
-              onFailure={this.responseGoogle}
-              cookiePolicy="single_host_origin"
-            />
-            <GoogleLogout
-              clientId={process.env.REACT_APP_CLIENT_ID}
-              buttonText="Sign Out"
-              onLogoutSuccess={this.signoutGoogle}
-            />
+            {currentUser ? (
+              <GoogleLogout
+                clientId={process.env.REACT_APP_CLIENT_ID}
+                buttonText="Sign Out"
+                onLogoutSuccess={this.signoutGoogle}
+              />
+            ) : (
+              <GoogleLogin
+                clientId={process.env.REACT_APP_CLIENT_ID}
+                buttonText="Sign In"
+                onSuccess={this.responseGoogle}
+                onFailure={this.responseGoogle}
+                cookiePolicy="single_host_origin"
+              />
+            )}
           </div>
         </div>
       </React.Fragment>
