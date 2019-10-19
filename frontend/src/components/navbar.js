a65ab1f70bcb6c3262010a86b436c3143a288fc3
Ban Vuong <vuongdieuban@gmail.com>
2019-10-18 19:21:38 -0600

    navbar collapsed

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -6,51 +6,54 @@ import { GoogleLogin, GoogleLogout } from "react-google-login";
 const NavBar = props => {
   const { onSigninSuccess, onSigninFail, onSignout, user } = props;
   return (
-    <Navbar bg="light" variant="light">
+    <Navbar bg="light" variant="light" expand="lg">
       <Navbar.Brand as={Link} to="/">
         Short URL
       </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/">
-          Home
-        </Nav.Link>
-        {user ? (
-          <GoogleLogout
-            render={renderProps => (
-              <Nav.Link
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-              >
-                Sign Out
-              </Nav.Link>
-            )}
-            buttonText="Sign Out"
-            clientId={process.env.REACT_APP_CLIENT_ID}
-            onLogoutSuccess={onSignout}
-          />
-        ) : (
-          <GoogleLogin
-            render={renderProps => (
-              <Nav.Link
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-              >
-                Sign In
-              </Nav.Link>
-            )}
-            clientId={process.env.REACT_APP_CLIENT_ID}
-            buttonText="Sign In"
-            onSuccess={onSigninSuccess}
-            onFailure={onSigninFail}
-            cookiePolicy="single_host_origin"
-          />
-        )}
-        {user && (
-          <Nav.Link as={Link} to="/me">
-            {user.name}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">
+            Home
           </Nav.Link>
-        )}
-      </Nav>
+          {user ? (
+            <GoogleLogout
+              render={renderProps => (
+                <Nav.Link
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  Sign Out
+                </Nav.Link>
+              )}
+              buttonText="Sign Out"
+              clientId={process.env.REACT_APP_CLIENT_ID}
+              onLogoutSuccess={onSignout}
+            />
+          ) : (
+            <GoogleLogin
+              render={renderProps => (
+                <Nav.Link
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  Sign In
+                </Nav.Link>
+              )}
+              clientId={process.env.REACT_APP_CLIENT_ID}
+              buttonText="Sign In"
+              onSuccess={onSigninSuccess}
+              onFailure={onSigninFail}
+              cookiePolicy="single_host_origin"
+            />
+          )}
+          {user && (
+            <Nav.Link as={Link} to="/me">
+              {user.name}
+            </Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
