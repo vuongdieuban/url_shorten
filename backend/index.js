7ccef29a3527c8b47327cf5786ad06fd5830c1ae
Ban Vuong <vuongdieuban@gmail.com>
2019-10-29 15:43:51 -0600

    change config file

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
+const config = require("config");
 const express = require("express");
 const app = express();
-
 // setup all the nesscessary startup here such as db connection, routes,..
 require("./startup/config")();
 require("./startup/db")();
 require("./startup/routes")(app);
 
 // Listen on Port
-const port = process.env.PORT || 5000;
+const port = config.get("Port") || 5000;
 const server = app.listen(port, () => console.log(`Listen on port ${port}`));
